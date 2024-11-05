a90f61998aaacb7e50e3f46696f5b7e3ab6df557
T K Santhosh Rao <120514816+Santhu4@users.noreply.github.com>
2024-11-05 17:26:38 +0530

    Time: 0 ms (100%), Space: 40.5 MB (29.31%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+      return helper(n,dp);
+    }
+    public int helper(int n,int dp[]){
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        dp[n] = helper(n-1,dp) + helper(n-2,dp);
+        return dp[n];
+    }
+}
